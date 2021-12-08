2ba841c1e18e6c81434f13e6538a513277d38d1d
Thomas <thomas.colin@le-campus-numerique.fr>
2021-12-08 14:51:46 +0100

    maj

--- .aws-sam/build/GetDevices/devices.py
+++ .aws-sam/build/GetDevices/devices.py
@@ -1,9 +1,25 @@
 import json
+import os
+import boto3
+from boto3.dynamodb.conditions import Key
+
+DEVICES_TABLE = os.environ['DEVICES_TABLE']
+dynamodb = boto3.resource('dynamodb')
 
 def get_devices(context, event):
-    return {
-        "statusCode": 400,
-        "body": json.dumps([{"id": "a", "devType": "co2", "name": "device1"},
-                {"id": "b", "devType": "temperature", "name": "device2"}]),
-        "headers": {'Access-Control-Allow-Origin': '*'}
+    if dynamodb:
+        table = dynamodb.Table(DEVICES_TABLE)
+        response = table.scan()
+        return {
+            "statusCode": 200,
+            "body": json.dumps(response["Items"]),
+            "headers": {'Access-Control-Allow-Origin': '*'}
         }
+    else:
+        return {
+            "statusCode": 400,
+            "body": {"nope"},
+            "headers": {'Access-Control-Allow-Origin': '*'}
+        }
+
+

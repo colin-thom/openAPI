26b08eb52351aeee7bbb9d49202bbc0bd48e4406
Thomas <thomas.colin@le-campus-numerique.fr>
2021-12-10 09:23:54 +0100

    valid

--- .aws-sam/build/UpdateDevice/devices.py
+++ .aws-sam/build/UpdateDevice/devices.py
@@ -1,10 +1,11 @@
 import json
+# import get_settings
 import os
 import boto3
+from botocore.exceptions import ClientError
 import uuid
-from boto3.dynamodb.conditions import Key
-from pprint import pprint
-from decimal import Decimal
+from boto3.dynamodb.conditions import Key, Attr
+
 
 DEVICES_TABLE = os.environ['DEVICES_TABLE']
 dynamodb = boto3.resource('dynamodb')
@@ -12,78 +13,78 @@ dynamodb = boto3.resource('dynamodb')
 def get_devices(context, event):
     if dynamodb:
         table = dynamodb.Table(DEVICES_TABLE)
-        response = table.scan()
-        return {
-            "statusCode": 200,
-            "body": json.dumps(response["Items"]),
-            "headers": {'Access-Control-Allow-Origin': '*'}
-        }
+        result = table.scan()
+        print(result)
+        return { 
+                "statusCode": 200, 
+                "body": json.dumps(result["Items"]),
+                "headers": {'Access-Control-Allow-Origin': '*'} 
+                }
     else:
-        return {
-            "statusCode": 400,
-            "body": {"nope"},
-            "headers": {'Access-Control-Allow-Origin': '*'}
-        }
+        print("else")
+        return { 
+                "statusCode": 400, 
+                "body": {"error": "error bitch"},
+                "headers": {'Access-Control-Allow-Origin': '*'} 
+                }
 
 def add_device(context, event):
     if dynamodb:
         table = dynamodb.Table(DEVICES_TABLE)
         params = context['multiValueQueryStringParameters']
-        response = table.put_item(
-                Item={
-                    'id': uuid.uuid4().hex,
-                    'deviceName': str(params['name'][0]),
-                    'deviceType': str(params["type"][0])
-                    }
-                )
+        status = 200
+        try:
+            result = table.put_item(
+                    Item={
+                        'deviceId': uuid.uuid4().hex, 
+                        'deviceName': str(params['deviceName'][0]), 
+                        'deviceType': str(params["deviceType"][0]),
+                        }
+                    )
+            result = json.dumps(result)
+        except ClientError as e:
+            result = e
+            status = 400
         return {
-            "statusCode": 200,
-            "body": {'ajouté correctement': 'ok'},
-            "headers": {'Access-Control-Allow-Origin': '*'}
-        }
-    else: 
-        return {
-            "statusCode": 400,
-            "body": {"nope"},
-            "headers": {'Access-Control-Allow-Origin': '*'}
-        }
+                "statusCode": status, 
+                "body": {"device add": result},
+                "headers": {'Access-Control-Allow-Origin': '*'} 
+                }
+
 
 def update_device(context, event):
     if dynamodb:
         table = dynamodb.Table(DEVICES_TABLE)
         params = context['multiValueQueryStringParameters']
-        response = table.update_item(
-            Key={
-                'id': str(params['id'][0]),
-            },
-            UpdateExpression="SET \
-                    deviceName= :n,\
-                    deviceType= :t",
-            ExpressionAttributeValues={
-                ':n': str(params['name'][0]),
-                ':t': str(params["type"][0])
-            },
-            ReturnValues="UPDATED_NEW"
-        )
-        return {
-            "statusCode": 200,
-            "body": {'modifié correctement' : 'ok'},
-            "headers": {'Access-Control-Allow-Origin': '*'}
-        }
+        result = table.update_item(
+                Key={'deviceId': str(params['deviceId'][0])},
+                UpdateExpression='SET deviceName = :name, deviceType = :type',
+                ExpressionAttributeValues={
+                        ':name': str(params['deviceName'][0]),
+                        ':type': str(params['deviceType'][0])
+                    }
+                )
+        return { 
+                "statusCode": 200, 
+                "body": {"device update": json.dumps(result)},
+                "headers": {'Access-Control-Allow-Origin': '*'} 
+                }
+    else:
+        print("else")
+        return "flute"
 
 def delete_device(context, event):
     if dynamodb:
         table = dynamodb.Table(DEVICES_TABLE)
         params = context['multiValueQueryStringParameters']
-        response = table.delete_item(
-            Key={
-                'id': str(params['id'][0]),
-            }
-        )
-        return {
-            "statusCode": 200,
-            "body": {'supprimé correctement' : 'ok'},
-            "headers": {'Access-Control-Allow-Origin': '*'}
-        }
-
-
+        result = table.delete_item(
+                Key={'deviceId': str(params['deviceId'][0])}
+                )
+        return { 
+                "statusCode": 200, 
+                "body": {"device delete": json.dumps(result)},
+                "headers": {'Access-Control-Allow-Origin': '*'} 
+                }
+    else:
+        print("else")
+        return "flute"
